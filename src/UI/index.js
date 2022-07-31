a715d54b936d17bb7a681c4a054155261414edc5
Aleksandr Ostromogilskii <ostromog@mail.ru>
2022-07-31 19:45:40 +0300

    global component registration

---
+++ src/UI/index.js
@@ -0,0 +1,11 @@
+import PostList from './PostList.vue'
+import PostItem from './PostItem.vue'
+import PostForm from './PostForm.vue'
+import LoadIcon from './LoadIcon.vue'
+
+export default [
+    PostList,
+    PostItem,
+    PostForm,
+    LoadIcon
+]
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import UIComponents from './UI'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+UIComponents.forEach((component)=>{
+    app.component(component.name, component)
+})
+app.mount('#app')
