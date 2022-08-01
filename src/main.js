bf1ff75e41d6b4b26940bc33b0e604b55996d908
Aleksandr Ostromogilskii <ostromog@mail.ru>
2022-08-01 18:53:47 +0300

    move UI folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import router from './router'
 import App from "./App.vue";
-import UIComponents from "./UI";
+import UIComponents from "./components/UI";
 
 const app = createApp(App);
 UIComponents.forEach((component) => {
